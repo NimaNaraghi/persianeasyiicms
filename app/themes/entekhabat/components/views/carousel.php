2ad919c5910368f4116226806398c7597339dccf
Nima Naraghi <nima.naraghi7@gmail.com>
2021-05-25 11:44:06 +0430

    Change Carousel image sizes.

--- app/themes/entekhabat/components/views/carousel.php
+++ app/themes/entekhabat/components/views/carousel.php
@@ -6,6 +6,6 @@ use app\modules\tfbcarousel\api\Carousel;
 
 <section class="section first-section">
     <div class="container">
-        <?= Carousel::widget('930', '476'); ?>
+        <?= Carousel::widget('1000', '512'); ?>
     </div>
 </section>
